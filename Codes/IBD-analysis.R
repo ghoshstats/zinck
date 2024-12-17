2275a2cae40e23c29be43051699484d818f501d2
Soham Ghosh <113257695+ghoshstats@users.noreply.github.com>
2024-12-17 02:37:55 -0600

    Update IBD-analysis.R

--- Codes/IBD-analysis.R
+++ Codes/IBD-analysis.R
@@ -75,6 +75,7 @@ Y <- ifelse(IBD_resp %in% c("CD", "UC"), 1, 0)
 
 X <- combined_otu
 
+########### (1) Zinck ############
 dlt <- rep(0,ncol(X)) ## Initializing the deltas with the sparsity of each column
 
 for(t in (1:ncol(X)))
@@ -107,23 +108,19 @@ theta <- fitIBD@sim[["est"]][["theta"]]
 beta <- fitIBD@sim[["est"]][["beta"]]
 X_tilde <- zinck::generateKnockoff(X,theta,beta,seed=1) ## Generating the kncokoff copy
 
-X_aug <- cbind(X,X_tilde) ## Creating the augmented matrix
+filter_zinck <- zinck.filter(as.matrix(X),as.matrix(X_tilde),as.factor(Y),
+                             model="Random Forest",offset=0,seed=312)
 
-######### Tuning the Random Forest model ####################
-bestmtry <- tuneRF(X_aug,as.factor(Y),stepFactor = 1.5,improve=1e-5,ntree=1000) ## Getting the best mtry hyperparameter
-m <- bestmtry[as.numeric(which.min(bestmtry[,"OOBError"])),1]
-# m = 109
-df_X <- as.data.frame(W_aug)
-colnames(df_X) <- NULL
-rownames(df_X) <- NULL
-df_X$Y <- Y
-set.seed(1)
-model_rf <- randomForest(formula=as.factor(Y)~.,ntree=1000,mtry=m,importance=TRUE,data=as.matrix(df_X)) ## Fitting the tuned Random Forest model
-cf <- as.data.frame(importance(model_rf))[,3] ## Extracting the Mean Decrease in Impurities for each variable
-W <- abs(cf[1:249])-abs(cf[250:498])
-T <- knockoff.threshold(W,fdr = 0.1, offset = 0) ## This is the knockoff threshold
-T <- zinck.threshold(W,fdr=0.1)
-names_zinck <- colnames(X[,which(W>=T)])
+selected_genera <- filter_zinck$selected
+
+
+## Importance scores ##
+W <- filter_zinck$W
+
+## Threshold ##
+T <- filter_zinck$T
+
+print(which(W>=T))
 
 # 2, 30,35, 36, 39, 55,58,69,77,79,90, 94 selected
 
@@ -140,7 +137,7 @@ colors <- ifelse(case_sums > control_sums, "red", "blue")
 
 
 # Create a vector to indicate which labels should be bold
-bold_indices <- c(3,10)
+bold_indices <- c(6,10)
 bold_labels <- ifelse(seq_along(names_zinck) %in% bold_indices, "bold", "plain")
 
 # Create a data frame for plotting
@@ -170,13 +167,111 @@ plt.species <- ggplot(data.species) +
 print(plt.species)
 dev.off()
 
+######### (2) MX-KF #########
+W <- log_normalize(X)
+set.seed(3)
+W_tilde.KF <- create.second_order(W)
+set.seed(46)
+imp <- stat.random_forest(W,W_tilde.KF,as.factor(Y))
+T <- knockoff.threshold(imp,fdr=0.1,offset=0)
+print(which(imp>=T))
+
+######### (3) LDA-KF ############
+df.LDA <- as(as.matrix(X),"dgCMatrix") ## Converting the matrix X into a dgCMatrix
+vanilla.LDA.comb <- LDA(df.LDA,k=20,method="VEM") ## Fitting LDA()
+theta.LDA.comb <- vanilla.LDA.comb@gamma
+beta.LDA.comb <- vanilla.LDA.comb@beta
+beta.LDA.comb <- t(apply(beta.LDA.comb, 1, function(row) row/sum(row))) ## Normalizing the posterior beta estimates
+X_tilde.LDA.comb <- zinck::generateKnockoff(X,theta.LDA.comb,beta.LDA.comb,seed=1) ## Standard LDA Knockoff matrix for X
+
+set.seed(2)
+W <- stat.random_forest(X,X_tilde.LDA.comb,as.factor(Y))
+T <- knockoff.threshold(W,fdr=0.1,offset=0)
+print(which(W>=T))
+
+########## (4) DeepLINK ##########
+
+```{python}
+import numpy as np
+import DeepLINK as dl
+import pandas as pd
+from PCp1_numFactors import PCp1 as PCp1
+import keras
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from pairwise_connected_layer import PairwiseConnected
+from itertools import combinations
+from keras.callbacks import EarlyStopping
+import tensorflow as tf
+import random
+import numpy as np
+
+aut_epoch = 100 # number of autoencoder training epochs
+aut_loss = 'mean_squared_error' # loss function used in autoencoder training
+aut_verb = 0 # verbose level of autoencoder
+mlp_epoch = 100 # number of mlp training epochs
+mlp_loss = 'binary_crossentropy'
+#mlp_loss = 'mean_squared_error' # loss function used in mlp training
+dnn_loss = 'binary_crossentropy'
+dnn_verb = 0
+aut_met = 'relu'
+dnn_met = 'elu'
+mlp_verb = 0 # verbose level of mlp
+l1 = 0.001 # l1 regularization factor in mlp
+lr = 0.001 # learning rate for mlp training
+
+X -= np.mean(X, axis=0)
+
+# Prevent division by zero: Replace zero std with 1 before dividing
+std_dev = np.std(X, axis=0, ddof=1)
+std_dev[std_dev == 0] = 1  # Avoid division by zero
+X /= std_dev
+
+# Normalize X1 while ensuring no division by zero in normalization
+norms = np.sqrt(np.sum(X ** 2, axis=0))
+norms[norms == 0] = 1  # Avoid division by zero
+X1 = X / norms
+r_hat = PCp1(X1, 15)
+random.seed(3)
+np.random.seed(3)
+tf.random.set_seed(3)
+Xnew = dl.knockoff_construct(X, r_hat, 'relu', aut_epoch, aut_loss, aut_verb)
+
+  # compute knockoff statistics
+
+p = Xnew.shape[1] // 2
+
+ # implement DeepPINK
+es = EarlyStopping(monitor='val_loss', patience=30, verbose=2)
+dp = Sequential()
+dp.add(PairwiseConnected(input_shape=(2 * p,)))
+dp.add(Dense(p, activation='elu', kernel_regularizer=keras.regularizers.l1(l1=l1)))
+dp.add(Dense(1, activation='relu',
+                    kernel_regularizer=keras.regularizers.l1_l2(l1=0.001, l2=0.001)))
+dp.compile(loss=mlp_loss, optimizer=keras.optimizers.Adam(learning_rate=lr))
+dp.fit(Xnew, y, epochs=mlp_epoch, batch_size=32, verbose=mlp_verb, validation_split=0.1, callbacks=[es])
+
+ # calculate knockoff statistics W_j
+weights = dp.get_weights()
+w = weights[1] @ weights[3]
+w = w.reshape(p, )
+z = weights[0][:p]
+z_tilde = weights[0][p:]
+W = (w * z) ** 2 - (w * z_tilde) ** 2
+  # feature selection
+np.random.seed(1)
+
+selected = dl.knockoff_select(W, 0.1, ko_plus=False)
+```
+
+
 ############################ Plotting the Venn Diagram ################################
 #######################################################################################
                       
 zinCK <- c(2, 30,35, 36, 39, 55,58,69,77,79,90, 94)
 DL <- c(12,17,18,30,36,44,49,52,70,88,90,94,96)
-KF <- c(2, 10, 12, 39, 49, 59, 64, 72, 88, 90)
-vanilla_LDA <- c(2, 30,36, 39,55,58,69,77, 90,94)
+KF <- c(2, 19, 39, 58, 77, 90)
+vanilla_LDA <- c(2, 30,35,36, 39,58,69,77, 90,94)
 venn.plot <- venn.diagram(
   x = list(zinCK = zinCK, KF = KF, DeepLINK = DL, vanilla_LDA = vanilla_LDA),
   category.names = c("Zinck" , "MX-KF" , "DeepLINK", "LDA-KF"),
