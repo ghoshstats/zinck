50adbbc42e94782d95b8b5c16765bc8d17cf8764
Soham Ghosh <113257695+ghoshstats@users.noreply.github.com>
2024-12-17 02:27:26 -0600

    Update CRC-analysis.R

--- Codes/CRC-analysis.R
+++ Codes/CRC-analysis.R
@@ -128,45 +128,34 @@ theta <- fitCRC@sim[["est"]][["theta"]]
 beta <- fitCRC@sim[["est"]][["beta"]]
 X_tilde_CRC <- zinck::generateKnockoff(X,theta,beta,seed=1) ## Generating the knockoff copy
 
-X_aug <- cbind(X,X_tilde_CRC) ## Creating the augmented matrix
+filter_zinck <- zinck.filter(as.matrix(X),as.matrix(X_tilde),as.factor(Y),
+                model="Random Forest",offset = 1,seed=1,mtry=28,
+                rftuning=TRUE,metric = "Gini")
 
-######### Tuning the Random Forest model ####################
+selected_species <- filter_zinck$selected
 
-bestmtry <- tuneRF(X_aug,as.factor(Y),stepFactor = 1.5,improve=1e-5,ntree=1000) ## Getting the best mtry hyperparameter
-m <- bestmtry[as.numeric(which.min(bestmtry[,"OOBError"])),1]
-df_X <- as.data.frame(X_aug)
-colnames(df_X) <- NULL
-rownames(df_X) <- NULL
-df_X$Y <- Y
-model_rf <- randomForest(formula=as.factor(Y)~.,ntree=1000,mtry=m,importance=TRUE,data=as.matrix(df_X)) ## Fitting the tuned Random Forest model
-cf <- as.data.frame(importance(model_rf))[,3] ## Extracting the Mean Decrease in Impurities for each variable
-W <- abs(cf[1:401])-abs(cf[402:802])
-T <- knockoff.threshold(W,fdr = 0.1, offset = 1) ## This is the knockoff threshold
+
+## Importance scores ##
+W <- filter_zinck$W
+
+## Threshold ##
+T <- filter_zinck$T
+
 print(which(W>=T))
-names_zinck <- colnames(X[,which(W>=T)])
 ## Selected Taxa :: 36,76,150,158,164,204,236,240,255,259,280,311,331,374
 
 ################# Method 2: Model-X Knockoff Filter ############################
 
 W_kf <- log_normalize(X)
-W_tilde_kf <- create.second_order(W_kf) # Generating the second order Gaussian knockoff copy
-W_aug <- cbind(W_kf,W_tilde_kf) ## Creating the augmented matrix
-
-######### Tuning the Random Forest model ####################
-bestmtry <- tuneRF(W_aug,as.factor(Y),stepFactor = 1.5,improve=1e-5,ntree=1000) ## Getting the best mtry hyperparameter
-m <- bestmtry[as.numeric(which.min(bestmtry[,"OOBError"])),1]
-df_X <- as.data.frame(W_aug)
-colnames(df_X) <- NULL
-rownames(df_X) <- NULL
-df_X$Y <- Y
 set.seed(1)
-model_rf <- randomForest(formula=as.factor(Y)~.,ntree=1000,mtry=m,importance=TRUE,data=as.matrix(df_X)) ## Fitting the tuned Random Forest model
-cf <- as.data.frame(importance(model_rf))[,1] ## Extracting the Mean Decrease in Impurities for each variable
-W <- abs(cf[1:401])-abs(cf[402:802])
-T <- knockoff.threshold(W,fdr = 0.1, offset = 0) ## This is the knockoff threshold
+W_tilde_kf <- create.second_order(W_kf) # Generating the second order Gaussian knockoff copy
+set.seed(45)
+W <- stat.random_forest(W_kf,W_tilde_kf,as.factor(Y))
+T <- knockoff.threshold(W,fdr=0.1,offset=0)
 print(which(W>=T))
 
-## Selected Taxa :: 36, 101, 150, 370
+## Selected Taxa :: 36, 76, 158, 331
+names_KF <- colnames(X[,which(W>=T)])
 
 ################# Method 3: Vanilla LDA model ##################################
 df.LDA <- as(as.matrix(X),"dgCMatrix")
@@ -177,24 +166,90 @@ beta.LDA.CRC <- t(apply(beta.LDA.CRC, 1, function(row) row/sum(row)))
 X_tilde_LDA.CRC <- zinck::generateKnockoff(X,theta.LDA.CRC,beta.LDA.CRC,seed=1) ## Generating the vanilla LDA knockoff copy
 X_aug <- cbind(X,X_tilde_LDA.CRC) ## Creating the augmented matrix
 
-######### Tuning the Random Forest model ####################
-bestmtry <- tuneRF(W_aug,as.factor(Y),stepFactor = 1.5,improve=1e-5,ntree=1000) ## Getting the best mtry hyperparameter
-m <- bestmtry[as.numeric(which.min(bestmtry[,"OOBError"])),1]
-df_X <- as.data.frame(W_aug)
-colnames(df_X) <- NULL
-rownames(df_X) <- NULL
-df_X$Y <- Y
-set.seed(1)
-model_rf <- randomForest(formula=as.factor(Y)~.,ntree=1000,mtry=m,importance=TRUE,data=as.matrix(df_X)) ## Fitting the tuned Random Forest model
-cf <- as.data.frame(importance(model_rf))[,3] ## Extracting the Mean Decrease in Impurities for each variable
-W <- abs(cf[1:401])-abs(cf[402:802])
-T <- knockoff.threshold(W,fdr = 0.1, offset = 0) ## This is the knockoff threshold
+set.seed(6)
+W <- stat.random_forest(X,X_tilde_LDA.CRC,Y)
+T <- knockoff.threshold(W,fdr=0.1,offset=1)
+print(which(W>=T))
+
+## Selected Taxa :: 5,  36, 150, 158, 164,204, 236, 255, 311, 331, 374
 names_LDA <- colnames(X[,which(W>=T)])
 
-## Selected Taxa :: 5,  36, 102, 150, 158, 164, 236, 255, 311, 331, 374
 
 ################## Method 4: DeepLINK ##########################################
 
+```{python}
+import numpy as np
+import DeepLINK as dl
+import pandas as pd
+from PCp1_numFactors import PCp1 as PCp1
+import keras
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from pairwise_connected_layer import PairwiseConnected
+from itertools import combinations
+from keras.callbacks import EarlyStopping
+import tensorflow as tf
+import random
+import numpy as np
+
+aut_epoch = 100 # number of autoencoder training epochs
+aut_loss = 'mean_squared_error' # loss function used in autoencoder training
+aut_verb = 0 # verbose level of autoencoder
+mlp_epoch = 100 # number of mlp training epochs
+mlp_loss = 'binary_crossentropy'
+#mlp_loss = 'mean_squared_error' # loss function used in mlp training
+dnn_loss = 'binary_crossentropy'
+dnn_verb = 0
+aut_met = 'relu'
+dnn_met = 'elu'
+mlp_verb = 0 # verbose level of mlp
+l1 = 0.001 # l1 regularization factor in mlp
+lr = 0.001 # learning rate for mlp training
+
+X -= np.mean(X, axis=0)
+
+# Prevent division by zero: Replace zero std with 1 before dividing
+std_dev = np.std(X, axis=0, ddof=1)
+std_dev[std_dev == 0] = 1  # Avoid division by zero
+X /= std_dev
+
+# Normalize X1 while ensuring no division by zero in normalization
+norms = np.sqrt(np.sum(X ** 2, axis=0))
+norms[norms == 0] = 1  # Avoid division by zero
+X1 = X / norms
+r_hat = PCp1(X1, 15)
+random.seed(4)
+np.random.seed(4)
+tf.random.set_seed(4)
+Xnew = dl.knockoff_construct(X, r_hat, 'relu', aut_epoch, aut_loss, aut_verb)
+
+  # compute knockoff statistics
+
+p = Xnew.shape[1] // 2
+
+ # implement DeepPINK
+es = EarlyStopping(monitor='val_loss', patience=30, verbose=2)
+dp = Sequential()
+dp.add(PairwiseConnected(input_shape=(2 * p,)))
+dp.add(Dense(p, activation='elu', kernel_regularizer=keras.regularizers.l1(l1=l1)))
+dp.add(Dense(1, activation='relu',
+                    kernel_regularizer=keras.regularizers.l1_l2(l1=0.001, l2=0.001)))
+dp.compile(loss=mlp_loss, optimizer=keras.optimizers.Adam(learning_rate=lr))
+dp.fit(Xnew, y, epochs=mlp_epoch, batch_size=32, verbose=mlp_verb, validation_split=0.1, callbacks=[es])
+
+ # calculate knockoff statistics W_j
+weights = dp.get_weights()
+w = weights[1] @ weights[3]
+w = w.reshape(p, )
+z = weights[0][:p]
+z_tilde = weights[0][p:]
+W = (w * z) ** 2 - (w * z_tilde) ** 2
+  # feature selection
+np.random.seed(1)
+
+selected = dl.knockoff_select(W, 0.1, ko_plus=False)
+```
+                        
 ## Selected Taxa :: 6,  19, 36, 106, 126, 161, 164, 234, 240, 241, 255, 294, 349
 names_DL <- colnames(X[,which(W>=T)])
 
@@ -226,7 +281,7 @@ control_sums <- colMeans(Xnorm[Y == 0, which(W>=T)])
 colors <- ifelse(case_sums > control_sums, "red", "blue")
 
 # Create a vector to indicate which labels should be bold
-bold_indices <- c(2,6,10,11)
+bold_indices <- c(10,11)
 bold_labels <- ifelse(seq_along(names_zinck) %in% bold_indices, "bold", "plain")
 
 # Create a data frame for plotting
@@ -304,8 +359,8 @@ plt.species
 ####### Set of selected taxa #######
 zinCK <- c(36,76,150,158,164,204,236,240,255,259,280,311,331,374)
 DL <- c(6,  19, 36, 106, 126, 161, 164, 234, 240, 241, 255, 294, 349)
-KF <- c(36, 101, 150, 370)
-vanilla_LDA <- c(5,  36, 102, 150, 158, 164, 236, 255, 311, 331, 374)
+KF <- c(36, 76, 158, 331)
+vanilla_LDA <- c(5,  36, 150, 158, 164,204, 236, 255, 311, 331, 374)
 
 venn.plot <- venn.diagram(
   x = list(zinCK = zinCK, KF = KF, DeepLINK = DL, vanilla_LDA = vanilla_LDA),
